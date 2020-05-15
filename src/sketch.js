755b1f995dd270ab930e89ca4163b33e69acfe78
elw396 <61168595+elw396@users.noreply.github.com>
2020-05-15 13:45:09 -0500

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -24,11 +24,45 @@ let frm = 0;
 
 let score = 0;
 
+let toolImagePaths = [
+  "tools/chop.png",
+  "tools/mix.png",
+  "tools/onecup.png",
+  "tools/spat.png",
+  "tools/spoon.png",
+  "tools/tea.png",
+  "tools/whisk.png"
+];
+let toolImages = [];
+let tx = [];
+let ty = [];
+
+export function preload() {
+  // load all tool images with the path in the array, "toolImagePaths"
+  for (let i=0; i<toolImagePaths.length; i++) {
+    toolImages[i] = loadImage(toolImagePaths[i]);
+  }
+}
+
+
+/*
+let img;
+img = loadImage("img.jpg");
+image(img, x, y);
+image(img, x, y, w, h);
+*/
+
 
 // p5js calls this code once when the page is loaded (and, during development,
 // when the code is modified.)
 export function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  for (let i=0; i<toolImages.length; i++) {
+    tx[i] = random(width);
+    ty[i] = random(height);
+  }
+
   video = select("video") || createCapture(VIDEO);
   video.size(width, height);
 
@@ -43,6 +77,8 @@ export function setup() {
 
   // Hide the video element, and just show the canvas
   video.hide();
+
+  imageMode(CENTER); //*****
 }
 
 // p5js calls this function once per animation frame. In this program, it does
@@ -54,33 +90,30 @@ export function draw() {}
 function drawPoses(poses) {
   // console.log(poses);
 
-// When the confidence score is more than 0.5, use rightWrist as the parameter
-if (poses.length > 0){
-  if (poses[0].pose.rightWrist.confidence > 0.5){
-    rwy = poses[0].pose.rightWrist.y;
-    rwx = width - poses[0].pose.rightWrist.x;
-  }else{
+  // When the confidence score is more than 0.5, use rightWrist as the parameter
+  if (poses.length > 0){
+    if (poses[0].pose.rightWrist.confidence > 0.5){
+      rwy = poses[0].pose.rightWrist.y;
+      rwx = width - poses[0].pose.rightWrist.x;
+    }else{
+      rwx = mouseX;
+      rwy = mouseY;
+    }
+    // When the confidence score is more than 0.5, use leftWrist as the parameter
+    if (poses[0].pose.leftWrist.confidence > 0.5){
+      lwy = poses[0].pose.leftWrist.y;
+      lwx = width - poses[0].pose.leftWrist.x;
+    }else{
+      lwx = width;
+      lwy = height;
+    }
+  } else{
     rwx = mouseX;
     rwy = mouseY;
-  }
-} else{
-  rwx = mouseX;
-  rwy = mouseY;
-}
-
-// When the confidence score is more than 0.5, use leftWrist as the parameter
-if (poses.length > 0){
-  if (poses[0].pose.leftWrist.confidence > 0.5){
-    lwy = poses[0].pose.leftWrist.y;
-    lwx = width - poses[0].pose.leftWrist.x;
-  }else{
     lwx = width;
     lwy = height;
   }
-} else{
-  lwx = width;
-  lwy = height;
-}
+
 
   // Modify the graphics context to flip all remaining drawing horizontally.
   // This makes the image act like a mirror (reversing left and right); this is
@@ -105,86 +138,159 @@ if (poses.length > 0){
   wrist(lwx, lwy, 20);
 
 
+
+
+
+  switch(ca){
+
+    case 0:
+    // Start page
+      startPage();
+      break;
+    case 1:
+      // recipe page
+      break;
+    case 2:
+
+      break;
+    case 3:
+      doYourOwn();
+      break;
+    case 4:
+      cookByRecipe();
+      break;
+  }
+
+  text(ca, 20, 30);
+}
+
+function startPage() {
   let sizey0 = (height/3)+height/6;
   let sizey1 = (height/3)+1.7*height/6;
   let sizey2 = (height/3)+2.5*height/5;
 
-
-  switch(ca){
-    // Start page
-    case 0:
-      // Display the Start, Settings and Exit
-      fill(255);
-      textAlign(CENTER, CENTER);
-      textFont('monospace');
-      // textSize(10);
-      textSize(int(width/10));
-      text("Chef to Chef", width/2, height/3);
-      textSize(width/30);
-      text("Cookbook", width/2, sizey0);
-
-      textSize(width/70);
-
-      text("Drag your hand over 'Cookbook' to begin", width/2, sizey2);
+  // Display the Start, Settings and Exit
+  fill(255);
+  textAlign(CENTER, CENTER);
+  textFont('monospace');
+  // textSize(10);
+  textSize(int(width/20));
+  text("Chef to Chef", width/2, height/3);
+  textSize(width/40);
+  text("Cook by recipe", width/2, sizey0);
+  text("Do your own", width/2, sizey1);
 
 
+  // The effect of putting your hand on the Recipe bar
+  if (sizey0-height/20 < rwy && rwy< sizey0 + height/20 && rwx > width*1/15 && rwx < width * 14/15) {
+    if (goin_b1) {
+      timer = frameCount;
+    }
+    goin_b1 = false;
+    noStroke();
+    fill(255, 50);
+    rectMode(CENTER);
+    rect(width/2, sizey0, width*7/8, height/10);
+    remaining = frameCount - timer;
 
-      // The effect of putting your hand on the Recipe bar
-      if (sizey0-height/20 < rwy && rwy< sizey0 + height/20 && rwx > width*1/15 && rwx < width * 14/15) {
-        if (goin_b1) {
-          timer = frameCount;
-        }
-        goin_b1 = false;
-        noStroke();
-        fill(255, 50);
-        rectMode(CENTER);
-        rect(width/2, sizey0, width*7/8, height/10);
-        remaining = frameCount - timer;
-
-        if (remaining < 50) {
-          fill(100);
-          arc(rwx, rwy, 80, 80, 0);
-        } else {
-          ca = 4;
-          frm = frameCount;
-          goin_b1 = true;
-        }
-      } else {
-        goin_b1 = true;
-      }
-
-      // The effect of putting your hand on the DIY bar
-      if (sizey1-height/20 < rwy && rwy< sizey1 + height/20 && rwx > width*1/15 && rwx < width * 14/15) {
-        if (goin_b2) {
-          timer = frameCount;
-        }
-        goin_b2 = false;
-        noStroke();
-        fill(255, 50);
-        rectMode(CENTER);
-        rect(width/2, sizey1, width*7/8, height/8);
-        remaining = frameCount - timer;
-
-        if (remaining < 50) {
-          fill(100);
-          arc(rwx, rwy, 80, 80, 0);
-        } else {
-          ca = 3;
-          goin_b2 = true;
-        }
-      } else {
-        goin_b2 = true;
-      }
-
-
-      break;
-
-    // recipe page
-    case 1:
-
+    if (remaining < 50) {
+      fill(100);
+      arc(rwx, rwy, 80, 80, 0);
+    } else {
+      ca = 4;
+      frm = frameCount;
+      goin_b1 = true;
+    }
+  } else {
+    goin_b1 = true;
   }
 
+  // The effect of putting your hand on the DIY bar
+  if (sizey1-height/20 < rwy && rwy< sizey1 + height/20 && rwx > width*1/15 && rwx < width * 14/15) {
+    if (goin_b2) {
+      timer = frameCount;
+    }
+    goin_b2 = false;
+    noStroke();
+    fill(255, 50);
+    rectMode(CENTER);
+    rect(width/2, sizey1, width*7/8, height/8);
+    remaining = frameCount - timer;
+
+    if (remaining < 50) {
+      fill(100);
+      arc(rwx, rwy, 80, 80, 0);
+    } else {
+      ca = 3;
+      goin_b2 = true;
+    }
+  } else {
+    goin_b2 = true;
+  }
 }
+
+function cookByRecipe() {
+  // *****
+  let potX = width/2;
+  let potY = height/2;
+  let potDia = 200;
+  noStroke();
+  fill(255, 150);
+  ellipse(potX, potY, potDia, potDia);
+
+  let counter = 0;
+  let detectArea = 150;
+  for (let i=0; i<toolImages.length; i++) {
+    // noStroke();
+    // fill(255, 150);
+    // ellipse(tx[i], ty[i], detectArea, detectArea);
+    image(toolImages[i], tx[i], ty[i], 100, 100);
+
+
+    let distance;
+    // check the distance between the wrist position and images' positions
+    distance = dist(rwx, rwy, tx[i], ty[i]);
+    if (distance < detectArea/2) {
+      // on the image
+      tx[i] = rwx;
+      ty[i] = rwy;
+    } else {
+      // not
+    }
+
+    // check the distance between the pot position and images' positions
+    distance = dist(potX, potY, tx[i], ty[i]);
+    if (distance < potDia/2) {
+      // on the image
+      //console.log(i);
+      if (i == 1 || i == 3) {
+        counter++;
+      }
+    } else {
+
+      // not
+    }
+  }
+  console.log(counter);
+  // if (counter == 2) {do something}
+}
+
+function doYourOwn() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function wrist(x, y, size){
   fill(map(x, 0, width, 0, 255), map(y, 0, height, 0 ,255), map(x + y, 0, width+height, 0,255));
